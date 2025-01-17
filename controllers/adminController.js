ff4efbe80cca970cca9b93fbee4ee4bb7fac25f5
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-17 16:54:45 +0530

    create login admin controller

---
+++ controllers/adminController.js
@@ -0,0 +1,63 @@
+import Admin from "../models/adminModel.js";
+import asyncHandler from "../middleware/asyncHandler.js";
+import generateToken from "../utils/jwt.js";
+
+export const createAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the fields");
+  }
+
+  const adminExists = await Admin.findOne({ email });
+
+  if (adminExists) {
+    throw new Error("Admin already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  try {
+    const newAdmin = await Admin.create({ username, email, hashedPassword });
+    generateToken(res, newAdmin._id);
+    await newAdmin.save();
+
+    res.status(201).json({
+      _id: newAdmin._id,
+      email: newAdmin.email,
+      username: newAdmin.username,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+export const loginAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new Error("Please fill all the fields");
+  }
+
+  const admin = await Admin.findOne({ email });
+  if (!admin) {
+    throw new Error("Admin not found");
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, admin.password);
+  if (!isPasswordCorrect) {
+    throw new Error("Incorrect password");
+  }
+
+  if (isPasswordCorrect) {
+    generateToken(res, admin._id);
+    res.status(200).json({
+      _id: admin._id,
+      email: admin.email,
+      username: admin.username,
+    });
+  } else {
+    res.status(401).json({ error: "Incorrect password" });
+  }
+});
