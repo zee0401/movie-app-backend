79decd0ad9e6b4448fba6b53831ab594f70659ab
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-21 16:21:52 +0530

    add edit movies added

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   getSortedMovies,
   getImdbTopMovies,
+  addMovie,
+  editMovie,
 } from "../controllers/moviesController.js";
 import { getAllMovies } from "../controllers/moviesController.js";
 import asyncHandler from "../middleware/asyncHandler.js";
@@ -11,5 +13,7 @@ router.get("/all-movies", getAllMovies);
 router.get("/sorted", asyncHandler(getSortedMovies));
 router.get("/sorted", getImdbTopMovies);
 router.get("/search", getSortedMovies);
+router.post("/create-movie", addMovie);
+router.put("/edit-movie", editMovie);
 
 export default router;
