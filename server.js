c830cffdf700849120bd85733334b9b90a115695
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-25 18:07:41 +0530

    bodyparse2r removed

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ import { connectDB } from "./config/connectDb.js";
 import moviesRoutes from "./routes/moviesRoutes.js";
 import { getSortedMovies } from "./controllers/moviesController.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import { corsConfig } from "./config/corsConfig.js";
+// import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(corsConfig());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(cookieParser());
 
 const PORT = process.env.PORT;
