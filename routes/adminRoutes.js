65e749883071a85e2a0963ec8978a44336e51cb1
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-01-27 15:13:57 +0530

    verify admin route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { loginAdmin, createAdmin } from "../controllers/adminController.js";
+import {
+  loginAdmin,
+  createAdmin,
+  verifyAdmin,
+} from "../controllers/adminController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 export const adminRoutes = Router();
 
 adminRoutes.post("/login", loginAdmin);
 adminRoutes.post("/create", createAdmin);
+adminRoutes.get("/verify-user", verifyAdmin);
 
 export default adminRoutes;
